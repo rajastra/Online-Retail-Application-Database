e6b78d82bc0c4172de8984709a77ad12fdb74efa
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-12 10:46:22 +0700

    menghapus tables

---
+++ drop_tables.sql
@@ -0,0 +1,6 @@
+-- Drop all tables
+DROP TABLE IF EXISTS OrderDetails;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Customers;
